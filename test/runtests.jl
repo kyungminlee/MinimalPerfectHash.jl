e147ece56d2ce2106079892cd8b765acdd4556c9
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-16 03:49:06 -0400

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,21 +7,21 @@ using Random
     for K in [Int, UInt8, UInt64, Float64]
         for V in [Int, Float64]
             for n in [0, 1, 2, 3, 4, 5, 8, 9, 27, 32, 33, 64, 65, 1024, 1025, 65536, 65537]
-                keys = collect(Set(Random.rand(K, n)))
-                vals = rand(V, length(keys))
-                chd = MinimalPerfectHash.CHD(keys, vals)
+                ks = collect(Set(Random.rand(K, n)))
+                vs = rand(V, length(ks))
+                chd = MinimalPerfectHash.CHD(ks, vs)
 
-                @test all(haskey(chd, k) for k in keys)
-                @test all((k => v) in chd for (k, v) in zip(keys, vals))
-                @test all(chd[k] == v for (k, v) in zip(keys, vals))
-                @test length(chd) == length(keys)
-                @test all(get(chd, k, nothing) == v for (k, v) in zip(keys, vals))
-                @test all(getkey(chd, k, nothing) == k for k in keys)
-                @test all(k in keys(chd) for k in keys)
+                @test all(haskey(chd, k) for k in ks)
+                @test all((k => v) in chd for (k, v) in zip(ks, vs))
+                @test all(chd[k] == v for (k, v) in zip(ks, vs))
+                @test length(chd) == length(ks)
+                @test all(get(chd, k, nothing) == v for (k, v) in zip(ks, vs))
+                @test all(getkey(chd, k, nothing) == k for k in ks)
+                @test all(k in keys(chd) for k in ks)
                 if n == 0
                     @test isempty(chd)
                 else
-                    @test sum(1 for (_, _) in chd) == length(keys)
+                    @test sum(1 for (_, _) in chd) == length(ks)
                 end
             end
         end

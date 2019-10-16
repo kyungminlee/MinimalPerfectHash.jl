afe1301e16cb8fdfe7d641457cf00f5577ae577e
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-16 12:22:09 -0400

    Fix copy

--- src/chdconstructor.jl
+++ src/chdconstructor.jl
@@ -92,6 +92,6 @@ end
 
 CHD(; kwargs...) = CHD{Any, Any}(; kwargs...)
 CHD(kv::Tuple{}; kwargs...) = CHD(; kwargs...)
-copy(d::CHD{K,V}) where {K, V} = CHD{K, V}(d.slots, d.keys, d.vals, d.count, d.r, d.indices)
+copy(d::CHD{K,V}) where {K, V} = CHD{K, V}(copy(d.slots), copy(d.keys), copy(d.vals), d.count, copy(d.r), copy(d.indices))
 CHD(ps::Pair{K, V}...; kwargs...) where {K, V} = CHD{K, V}(ps; kwargs...)
 CHD(ps::Pair...; kwargs...)                    = CHD(ps; kwargs...)

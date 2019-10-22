c435dc81f7336c06d70e964ccaabd6e71aa11f18
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-10-21 20:19:34 -0400

    @inline and fix getindex bug

--- src/chd.jl
+++ src/chd.jl
@@ -15,30 +15,30 @@ function chd_keyindex(c ::CHD{K, V}, key ::K) ::Int where {K, V}
   r0 = c.r[1]
   h = hash(key) ⊻ r0
   i = h % UInt64(length(c.indices))
-  ri = c.indices[i+1]
+  @inbounds ri = c.indices[i+1]
 
   ri >= UInt16(length(c.r)) && return -1
 
-  r = c.r[ri+1]
+  @inbounds r = c.r[ri+1]
   ti = (h ⊻ r) % UInt64(length(c.keys))
 
-  (!c.slots[ti+1]) && return -1
-  (c.keys[ti+1] != key) && return -1
+  @inbounds (!c.slots[ti+1]) && return -1
+  @inbounds (c.keys[ti+1] != key) && return -1
 
   return ti+1
 end
 
-function Base.getindex(c ::CHD{K, V}, key) where {K, V}
+@inline function Base.getindex(c ::CHD{K, V}, key) where {K, V}
   ti = chd_keyindex(c, K(key))
-  @boundscheck ti == 0 && throw(KeyError("key $key not found"))
+  @boundscheck ti <= 0 && throw(KeyError("key $key not found"))
   return c.vals[ti]
 end
 
-function Base.length(c ::CHD) ::Int
+@inline function Base.length(c ::CHD) ::Int
   return c.count
 end
 
-function Base.iterate(c ::CHD{K, V}, count::Int=1) ::Union{Nothing, Tuple{Pair{K, V}, Int}} where {K, V}
+@inline function Base.iterate(c ::CHD{K, V}, count::Int=1) ::Union{Nothing, Tuple{Pair{K, V}, Int}} where {K, V}
   # skip empty slots
   while count <= length(c.keys) && !c.slots[count]
     count += 1
@@ -47,17 +47,17 @@ function Base.iterate(c ::CHD{K, V}, count::Int=1) ::Union{Nothing, Tuple{Pair{K
   return ((c.keys[count] => c.vals[count]), count+1)
 end
 
-function Base.in(k, keys ::Base.KeySet{K, CHD{K,V}}) where {K, V}
+@inline function Base.in(k, keys ::Base.KeySet{K, CHD{K,V}}) where {K, V}
   ti = chd_keyindex(keys.dict, K(k))
   return ti > 0
 end
 
-function Base.get(h::CHD{K,V}, key, default) where {K, V}
+@inline function Base.get(h::CHD{K,V}, key, default) where {K, V}
     index = chd_keyindex(h, key)
     @inbounds return (index <= 0) ? default : h.vals[index]::V
 end
 
-function Base.getkey(h::CHD{K,V}, key, default) where {K, V}
+@inline function Base.getkey(h::CHD{K,V}, key, default) where {K, V}
     index = chd_keyindex(h, key)
     @inbounds return (index <= 0) ? default : h.keys[index]::K
 end
